a0f24c8fd23b4dec751605bf716f209a9ddc8101
Joel <itsjoelpm@gmail.com>
2022-12-07 20:20:14 +0300

    chore: create-wallet function

---
+++ src/common/wallet.ts
@@ -0,0 +1,42 @@
+import Arweave from 'arweave';
+import { generateMnemonic, getKeyFromMnemonic } from 'arweave-mnemonic-keys';
+import { CreateProps, CreateReturnProps } from '../types/wallet';
+
+const arweave = Arweave.init({
+  host: '127.0.0.1',
+  port: 8080,
+  protocol: 'http',
+});
+
+/**
+ * create wallet
+ * @params options -> { seedPhrase: boolean }
+ * @returns walletAddress, JWK, and seedPhrase if options.seedPhrase is passed in
+ */
+
+export async function createWallet(
+  options: CreateProps
+): Promise<CreateReturnProps> {
+  if (options && options.seedPhrase) {
+    const seedPhrase = await generateMnemonic();
+
+    if (seedPhrase) {
+      const key = await getKeyFromMnemonic(seedPhrase);
+      const walletAddress = await arweave.wallets.jwkToAddress(key);
+
+      return {
+        key,
+        walletAddress,
+        seedPhrase,
+      };
+    }
+  }
+
+  const key = await arweave.wallets.generate();
+  const walletAddress = await arweave.wallets.jwkToAddress(key);
+
+  return {
+    key,
+    walletAddress,
+  };
+}
