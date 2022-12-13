ad3ad51b7f2c8ae147e02b4b71183371a0dd1bd2
Joel <itsjoelpm@gmail.com>
2022-12-13 20:17:07 +0300

    chore: move tests folder to root

--- __tests__/wallet.spec.ts
+++ __tests__/wallet.spec.ts
@@ -1,7 +1,7 @@
 const assert = require('assert');
-const { createWallet } = require('../common/wallet');
+const { createWallet, getAddress } = require('../src/common/wallet');
 
-describe('Wallet Functions', () => {
+describe('Create Wallet', () => {
   jest.setTimeout(300000); // takes a while to generate seedPhrase
 
   it('should create wallet without options.seedPhrase passed in', async () => {
@@ -18,5 +18,20 @@ describe('Wallet Functions', () => {
     expect(generateWallet.key).toBeDefined();
     expect(generateWallet.seedPhrase).toBeDefined();
     expect(generateWallet.walletAddress).toBeDefined();
+  });
+});
+
+describe('Get Address', () => {
+  jest.setTimeout(300000);
+
+  it('should return wallet address', async () => {
+    const { key } = await createWallet();
+
+    if (key) {
+      const address = await getAddress(key);
+
+      expect(address).toBeDefined();
+      expect(typeof address).toBe('string');
+    }
   });
 });
