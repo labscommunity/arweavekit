3072ba7ecec59a5017447646942f433bb475968c
Joel <itsjoelpm@gmail.com>
2022-12-19 02:47:02 +0300

    chore: function

---
+++ src/common/transaction.ts
@@ -0,0 +1,55 @@
+import Arweave from 'arweave';
+import { SignTransactionProps } from '../types/transaction';
+
+const arweave = Arweave.init({
+  host: '127.0.0.1',
+  port: 8080,
+  protocol: 'http',
+});
+
+/**
+ * sign transaction
+ * @returns trasaction data
+ */
+export async function signTransaction(input: SignTransactionProps) {
+  if (!input?.transaction && !input?.key) {
+    // generate key
+    const key = await arweave.wallets.generate();
+    let transaction;
+
+    // create data transaction
+    if (input.options.type === 'data') {
+      const { options } = input;
+      transaction = await arweave.createTransaction(
+        {
+          data: options.data,
+        },
+        key
+      );
+    } else if (input.options.type === 'wallet') {
+      // sign wallet to wallet transaction
+      const { address, quantity } = input.options;
+      transaction = await arweave.createTransaction({
+        target: address,
+        quantity: arweave.ar.arToWinston(quantity as string),
+      });
+    }
+
+    // sign transaction
+    if (transaction) {
+      const signedTransaction = await arweave.transactions.sign(
+        transaction,
+        key
+      );
+      return signedTransaction;
+    }
+  }
+
+  if (input.transaction && input.key) {
+    const signedTransaction = arweave.transactions.sign(
+      input.transaction,
+      input.key
+    );
+    return signedTransaction;
+  }
+}
