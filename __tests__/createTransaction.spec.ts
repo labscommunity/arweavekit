6c04b84ce4ed3f6e2cb0436e4800a58f6145d6ee
Ropats <rohitcpathare@gmail.com>
2022-12-26 10:05:07 +0530

    Add test for wallet to wallet transaction

--- __tests__/createTransaction.spec.ts
+++ __tests__/createTransaction.spec.ts
@@ -35,7 +35,15 @@ describe('Create Transaction', () => {
     expect(txn).toEqual('Pass in valid data or token quantity and target to create a transaction.')
   });
 
-  it('should return ___ when target and quantity, but no key arguments are passed in on function call', async () => { });
+  it('should return ___ when target and quantity, but no key arguments are passed in on function call', async () => {
+    const txn = await createTransaction({ target: 'zBBCc4yIvbs2kS2t1ZsAhjThkC15P2Y5ICryqN2agus', quantity: '1000000' });
+
+    expect(txn).toMatchObject({
+      owner: '',
+      target: 'zBBCc4yIvbs2kS2t1ZsAhjThkC15P2Y5ICryqN2agus',
+      quantity: '1000000'
+    })
+  });
 
   it('should return ___ when target, quantity and key arguments are passed in on function call', async () => { });
 
