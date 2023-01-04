582b5d5032a14a5beaf17a07fbccf72fa2ffc72f
Joel <itsjoelpm@gmail.com>
2023-01-04 15:48:29 +0300

    fix: getBalance return type

--- src/common/wallet.ts
+++ src/common/wallet.ts
@@ -58,7 +58,7 @@ export async function getAddress(key: JWKInterface): Promise<string> {
 
 /**
  * create wallet
- * @params options -> { walletAddress: string }
+ * @params address: string
  * @returns walletBalance: string
  */
 
