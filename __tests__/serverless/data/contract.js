1e6a13612e306297ccd68b2c0ab7fe3c6c2ed707
Joel <itsjoelpm@gmail.com>
2023-01-04 15:46:42 +0300

    chore: test smart contract

--- __tests__/serverless/data/contract.js
+++ __tests__/serverless/data/contract.js
@@ -1,4 +1,4 @@
-export function handler(state, action) {
+export async function handle(state, action) {
   if (action.input.function === 'createPost') {
     const posts = state.posts
     posts.push(action.input.post)
