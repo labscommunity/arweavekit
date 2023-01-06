66bc799e9db29e7c1e3092fab3d45d86dad31f1a
Joel <itsjoelpm@gmail.com>
2023-01-06 19:53:50 +0300

    chore: make changes to create txn function

--- src/common/transaction.ts
+++ src/common/transaction.ts
@@ -1,6 +1,5 @@
 import Arweave from 'arweave';
 import Bundlr from '@bundlr-network/client';
-import { JWKInterface } from 'arweave/node/lib/wallet';
 import { CreateTransactionProps } from '../types/transaction';
 import { getAddress, getBalance } from './wallet';
 
@@ -14,27 +13,15 @@ const arweaveMainnet = Arweave.init({
  * create wallet
  * @params
  * data?: string | Uint8Array | ArrayBuffer;
+ * quantity?: string;
+ * target?: string;
  * options?: {
-    tags: Tag[];
-    quantity?: string;
-    target?: string;
-    uploadLargeBatch?: boolean
+    tags?: Tag[];
+    useBundlr?: boolean;
+    signAndPost: boolean
   };
  * key?: JWKInterface;
- * @returns
- * format: number;
- * id: string,
- * last_tx: string;
- * owner: string;
- * tags: Tag[];
- * target: string;
- * quantity: string;
- * data: string | Uint8Array | ArrayBuffer;
- * data_size: string;
- * data_root: string;
- * data_tree: [];
- * reward: string;
- * signature: string;
+ * @returns Arweave Transaction
  */
 
 /* 
@@ -46,16 +33,20 @@ Rethinking logic defaulting to bundlr
 - tags
 */
 
-
-export async function createTransaction(
-  params: CreateTransactionProps) {
+export async function createTransaction(params?: CreateTransactionProps) {
   // Check is transaction is for data or wallet to wallet
-  if (params.data) {
+  if (params?.data) {
     // Check whether to use @bundlr-network/client or arweave
     if (params.options?.useBundlr) {
-      const bundlr = new Bundlr("http://node2.bundlr.network", "arweave", params.key);
-      const txn = bundlr.createTransaction(JSON.stringify(params.data), { tags: params.options.tags });
-      if (params.options?.signAndPostTxn) {
+      const bundlr = new Bundlr(
+        'http://node2.bundlr.network',
+        'arweave',
+        params.key
+      );
+      const txn = bundlr.createTransaction(JSON.stringify(params.data), {
+        tags: params.options.tags,
+      });
+      if (params.options?.signAndPost) {
         await txn.sign();
         const response = await txn.upload();
         return response;
@@ -63,11 +54,14 @@ export async function createTransaction(
         return txn;
       }
     } else {
-      const txn = await arweaveMainnet.createTransaction({
-        data: params.data,
-      }, params.key ? params.key : 'use_wallet');
+      const txn = await arweaveMainnet.createTransaction(
+        {
+          data: params.data,
+        },
+        params.key ? params.key : 'use_wallet'
+      );
       params.options?.tags?.map((k, i) => txn.addTag(k.name, k.value));
-      if (params.options?.signAndPostTxn) {
+      if (params.options?.signAndPost) {
         await arweaveMainnet.transactions.sign(txn, params.key);
         const response = await arweaveMainnet.transactions.post(txn);
         return { txn, response };
@@ -75,19 +69,22 @@ export async function createTransaction(
         return txn;
       }
     }
-  } else if (params.target && params.quantity) {
+  } else if (params?.target && params?.quantity) {
     let senderBalance: string = '';
     if (params.key) {
       const senderAddress = await getAddress(params.key);
-      senderBalance = await getBalance({ walletAddress: senderAddress });
-    };
+      senderBalance = await getBalance(senderAddress);
+    }
     if (parseInt(senderBalance) >= parseInt(params.quantity)) {
-      const txn = await arweaveMainnet.createTransaction({
-        target: params.target,
-        quantity: params.quantity,
-      }, params.key ? params.key : 'use_wallet');
+      const txn = await arweaveMainnet.createTransaction(
+        {
+          target: params.target,
+          quantity: params.quantity,
+        },
+        params.key ? params.key : 'use_wallet'
+      );
       params.options?.tags?.map((k, i) => txn.addTag(k.name, k.value));
-      if (params.options?.signAndPostTxn) {
+      if (params.options?.signAndPost) {
         await arweaveMainnet.transactions.sign(txn, params.key);
         const response = await arweaveMainnet.transactions.post(txn);
         return { txn, response };
@@ -95,22 +92,25 @@ export async function createTransaction(
         return txn;
       }
     } else {
-      return 'Wallet does not have sufficient balance to complete transaction.'
+      return 'Wallet does not have sufficient balance to complete transaction.';
     }
-  } else if (params.data && params.target && params.quantity) {
+  } else if (params?.data && params?.target && params?.quantity) {
     let senderBalance: string = '';
     if (params.key) {
       const senderAddress = await getAddress(params.key);
-      senderBalance = await getBalance({ walletAddress: senderAddress });
-    };
+      senderBalance = await getBalance(senderAddress);
+    }
     if (parseInt(senderBalance) >= parseInt(params.quantity)) {
-      const txn = await arweaveMainnet.createTransaction({
-        data: params.data,
-        target: params.target,
-        quantity: params.quantity,
-      }, params.key ? params.key : 'use_wallet');
+      const txn = await arweaveMainnet.createTransaction(
+        {
+          data: params.data,
+          target: params.target,
+          quantity: params.quantity,
+        },
+        params.key ? params.key : 'use_wallet'
+      );
       params.options?.tags?.map((k, i) => txn.addTag(k.name, k.value));
-      if (params.options?.signAndPostTxn) {
+      if (params.options?.signAndPost) {
         await arweaveMainnet.transactions.sign(txn, params.key);
         const response = await arweaveMainnet.transactions.post(txn);
         return { txn, response };
@@ -118,9 +118,10 @@ export async function createTransaction(
         return txn;
       }
     } else {
-      return 'Wallet does not have sufficient balance to complete transaction.'
+      return 'Wallet does not have sufficient balance to complete transaction.';
     }
+  } else {
+    // When neither data nor token quantity and target are provided
+    return 'Pass in valid data or token quantity and target to create a transaction.';
   }
-  // When neither data nor token quantity and target are provided
-  return 'Pass in valid data or token quantity and target to create a transaction.'
 }
