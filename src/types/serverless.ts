68a4edb75ae2c8f73cbf24ce4db769a7e6534d18
Joel <itsjoelpm@gmail.com>
2023-02-20 15:03:01 +0300

    chore: auth functions

---
+++ src/types/serverless.ts
@@ -0,0 +1,23 @@
+export interface CreateServerlessProps {
+  token: string;
+  functionSource: Uint8Array;
+  initialState: string | object;
+}
+
+export interface CreateServerlessReturnProps {
+  functionId: string;
+  functionUrl: string;
+  functionSource: string;
+  result: {
+    status: number;
+    statusText: string;
+  };
+}
+export interface ReadserverlessProps {
+  token: string;
+  functionId: string;
+}
+
+export interface WriteserverlessProps extends ReadserverlessProps {
+  inputs: {};
+}
