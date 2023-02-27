83a05424d45093dbdc82fe39d2bf79627db2f18c
Joel <itsjoelpm@gmail.com>
2023-02-27 13:35:46 +0300

    chore: refactor wallet types

--- src/types/wallet.ts
+++ src/types/wallet.ts
@@ -2,14 +2,24 @@ import { JWKInterface } from 'arweave/node/lib/wallet';
 
 export interface CreateProps {
   seedPhrase?: boolean;
-  environment?: 'local' | 'mainnet';
+  environment: 'testnet' | 'mainnet';
 }
 export interface CreateReturnProps {
   key: JWKInterface;
   walletAddress: string;
   seedPhrase?: string;
 }
+
+export interface GetAddressProps {
+  key: JWKInterface;
+  environment: 'testnet' | 'mainnet';
+}
+
 export interface GetBalanceProps {
   address: string;
-  environment?: 'local' | 'mainnet';
+  environment: 'testnet' | 'mainnet';
+  options?: {
+    trim?: boolean;
+    format?: boolean;
+  };
 }
