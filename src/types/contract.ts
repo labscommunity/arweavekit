7c577c205d3a186fea8a96c7996156cba01e0f25
Joel <itsjoelpm@gmail.com>
2023-02-28 17:23:45 +0300

    add read and write fns

--- src/types/contract.ts
+++ src/types/contract.ts
@@ -1,17 +1,30 @@
-import { ArWallet, Contract } from 'warp-contracts';
+import { ArWallet, Contract, CustomSignature } from 'warp-contracts';
 
-export interface CreateProps {
+export interface CreateContractProps {
   wallet: ArWallet;
   initialState: string;
   contractSource: string;
   environment: 'local' | 'testnet' | 'mainnet';
 }
 
-export interface CreateReturnProps {
+export interface CreateContractReturnProps {
   status: {
     code: number;
     message: string;
   };
   contractTxId: string;
   contract: Contract;
+}
+
+export interface WriteContractProps {
+  environment: 'local' | 'testnet' | 'mainnet';
+  contractTxId: string;
+  wallet: ArWallet | CustomSignature;
+  options: {};
+}
+
+export interface ReadContractProps {
+  environment: 'local' | 'testnet' | 'mainnet';
+  contractTxId: string;
+  wallet: ArWallet | CustomSignature;
 }
